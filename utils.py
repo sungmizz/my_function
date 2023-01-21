79afd0e559d3ad88fe3bf32a5eb71da3f0f1d319
sungmizz <sungmizz@naver.com>
2023-01-22 05:07:10 +0900

    함수 만들었어요!

---
+++ utils.py
@@ -0,0 +1,20 @@
+# 한 줄 짜리 주석
+"""
+코드 작성 일자 : 2023년 1월 22일
+코드 작성자 : 박성미
+코드 이름 : utils.py
+코드 목적 : study
+"""
+
+
+# 함수의 앞에는 빈줄이 2줄 필요함
+# 코드의 맨 끝에는 한줄만 필요
+
+def factorial(x):
+    if x <= 1:
+        return 1
+    return x * factorial(x-1)
+
+
+def pp():
+    print("hello")
